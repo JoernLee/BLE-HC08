3e2f6c4fbebe692cb278b9e461728be7dc92beae
Joern <njljx200778@gmail.com>
2017-08-09 20:17:54 +0800

    Initialization for HC-08 Bluetooth

---
+++ app/build/generated/source/buildConfig/debug/com/huicheng/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.huicheng;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.huicheng";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.2";
+}
